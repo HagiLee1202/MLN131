c9e7ee9fbe658215328eff6496fba1b3bdea054d
HagiLee <cungthugaming@gmail.com>
2026-01-29 15:05:44 +0700

    Remove old backup files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './tailwind.css'
 import './index.css'
-import './animations.css'
 import App from './App.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 
